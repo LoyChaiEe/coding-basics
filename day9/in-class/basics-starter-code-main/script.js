98e5deca92266b4a00b02112ac9ae0b4b9f71147
LoyChaiEe <celoy001@gmail.com>
2022-06-19 18:10:36 +0800

    editted the logic for wild card generation

--- day9/in-class/basics-starter-code-main/script.js
+++ day9/in-class/basics-starter-code-main/script.js
@@ -1,5 +1,15 @@
 var deck = []
 var gamemode = "set-up"
+const suit = ["❤️", "♦", "♣", "♠️"]
+const cardName = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", ]
+function Card(cardname, logo, number){
+  this.name = cardname;
+  this.suit = logo;
+  this.rank = number
+}
+var playerChoiceWildCard = false
+var wildCardEnabled = false
+var wildCard = new Card("", "", 0)
 
 // Shuffle the deck and save it in a new variable shuffledDeck
 // to communicate that we have shuffled the deck.
@@ -8,8 +18,7 @@ var main = function (input) {
   var output = ""
   switch(gamemode){
     case "set-up":
-      setup()
-      output = `Welcome to Moar Cards!<br<br>Choose which mode you want to play:<br><br>1. High card<br>2. Low Card<br><br>Type in the number to choose`
+      output = `Welcome to Moar Cards!<br<br>Choose which mode you want to play:<br><br>1. High card<br>2. Low Card<br><br>Type in the number to choose.<br><br>` + setup()
       gamemode = "Select Mode"
       break;
     case "Select Mode":
@@ -20,18 +29,33 @@ var main = function (input) {
       break;
     case "Low Card":
       output = game()
-
   }
+  return output
 };
 var selectMode = function(userChoice){
+  let output = ""
+  switch(userChoice){
+    case 1:
+      gamemode = "High Card"
+      output += `You have chose ${gamemode}. Press play to continue`
+      break
+    case 2:
+      gamemode = "Low Card"
+      output += `You have chose ${gamemode}. Press play to continue`
+      break
+  }
 
 }
 var game = function(){
   let myOutputValue = ""
   let playerHand = []
   let sizeOfHand = ""
-  while(isNaN(sizeOfHand) === false && Number.isInteger(sizeOfHand) == false)
-  prompt("How many cards")
+  do{
+    sizeOfHand = Number(prompt("How many cards do you want to draw", ""))
+  }while(isNaN(sizeOfHand) === true || Number.isInteger(sizeOfHand) == false || sizeOfHand == "" || sizeOfHand == 0)
+  for(let i = 0; i < sizeOfHand; i++){
+
+  }
   let computerCard = shuffledDeck.pop();
   // Construct an output string to communicate which cards were drawn
   // Compare computer and player cards by rank attribute
@@ -53,20 +77,49 @@ var game = function(){
 //set-up
 var setup = function(){
   makeDeck()
+  let output = ""
   deck = [...shuffleCards(deck)]
+  let playerChoice = ""
+  let wildCardChoice = ""
+  do{
+    wildCardChoice = prompt("Do you want wild card to be enabled? Type in yes or no", "")
+    wildCardChoice = wildCardChoice.toLowerCase()
+  }while(wildCardChoice != "yes" && wildCardChoice != "no")
+  if(wildCardChoice == "yes")
+   wildCardEnabled = true
+  while(playerChoice != "yes" && playerChoice != "no" && wildCardEnabled){
+    playerChoice = prompt("Do you want to choose the wild card? Type in yes or no", "")
+    playerChoice = playerChoice.toLowerCase()
+    console.log(playerChoice != "yes" && playerChoice != "no")
+    console.log(wildCardEnabled)
+  }
+  if(playerChoice == "yes")
+   playerChoiceWildCard = true
+  if(wildCardEnabled)
+  {
+    if(playerChoiceWildCard){
+      let wildCardSuitnum = 0
+      do{
+        wildCard.name = prompt("Name of card:", "")
+      }while(cardName.includes(wildCard.name) == false)
+      do{
+        wildCardSuitnum = Number(prompt("Choose the suit:\n\n1. Hearts\n2. Diamonds\n3. Clubs\n4. Spades\n\nType in the number 1 to 4", ""))
+      }while(wildCardSuitnum != 1 && wildCardSuitnum != 2 && wildCardSuitnum != 3 && wildCardSuitnum != 4)
+      wildCard.suit = suit[wildCardSuitnum]
+    }
+    else{
+      wildCard.name = cardName[Math.floor(Math.random()*cardName.length)]
+      wildCard.suit = suit[Math.floor(Math.random()*suit.length)]
+    }
+    output += `Wild card enabled is ${wildCardEnabled}. The wild card is ${wildCard.name} of ${wildCard.suit}.`
+  }
+  return output
 }
 var makeDeck = function(){
-  var suit = ["❤️", "♦", "♣", "♠️"]
-  var name = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", ]
-  function Card(cardName, logo, number){
-    this.name = cardName;
-    this.suit = logo;
-    this.rank = number
-  }
   for(let i = 0; i < suit.length; i++){
-    for(let j = 0; j < name.length; j++)
+    for(let j = 0; j < cardName.length; j++)
     {
-      let currentCard = new Card(name[j], suit[i], j + 1)
+      let currentCard = new Card(cardName[j], suit[i], j + 1)
       deck.push(currentCard)
     }
   }
@@ -85,7 +138,7 @@ var cardComparator = function(card1, card2){
   if(card1.rank === card2.rank){
     return false
   }
-  else if (card1.rank > card2.rank) {
+  else if (card1.rank > card2.rank || (card1.name == wildCard.name && card1.suit == wildCard.suit)) {
     // Add conditional-dependent text to the output string
     return true
     // Else if computer card rank is less than player card rank, player wins
